6ae09151161730f3801376f6b4beeb73d05594cb
A Weak Developer <aweakdeveloper@gmail.com>
2019-01-07 15:50:13 +0800

    + init project from template

---
+++ MySchool/Data/SchoolContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MySchool.Models;
+
+namespace MySchool.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
